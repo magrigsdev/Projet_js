10e007d0da057fa6c9738236ea277f97829647cb
= <=>
2022-04-08 12:37:01 +0200

    game

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-let liens = document.querySelectorAll("a:not(.active)");
+let liens = document.querySelectorAll("a[href='##']");
 
 for (let index = 0; index < liens.length; index++) {
     const element = liens[index];
